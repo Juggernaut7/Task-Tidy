30f8f24d1aab163a371e6f0610fc06a483ce8763
Juggernaut7 <abdulkabir0600@gmail.com>
2025-07-19 16:15:35 +0100

    file path config

--- tasktidy-frontend/src/services/api.js
+++ tasktidy-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 // import { getAuthToken } from '../utils/auth'; // Uncomment if backend requires a token
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://task-tidy-4wgp.vercel.app";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
